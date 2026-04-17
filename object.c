783b15af9ea8842f44daad928f6b41ebe19f056b
Yesh-2516 <yeshaswinie.d@gmail.com>
2026-04-17 13:40:18 +0000

    Add object_write() skeleton and header construction

--- object.c
+++ object.c
@@ -95,8 +95,23 @@ int object_exists(const ObjectID *id) {
 // Returns 0 on success, -1 on error.
 int object_write(ObjectType type, const void *data, size_t len, ObjectID *id_out) {
     // TODO: Implement
-    (void)type; (void)data; (void)len; (void)id_out;
-    return -1;
+    const char *type_str;
+    switch(type) {
+	case OBJ_BLOB: type_str="blob"; break;
+	case OBJ_TREE: type_str="tree"; break;
+	case OBJ_COMMIT: type_str="commit"; break;
+	default: return -1;
+    }
+
+    char header[64];
+    int header_len=snprintf(header,sizeof(header),"%s %zu",type_str, len);
+    size_t full_len=(size_t)header_len+1+len;
+    uint8_t *full_obj=malloc(full_len);
+    if(!full_obj) return -1;
+
+    memcpy(full_obj,header,header_len);
+    full_obj[header_len]='\0';
+    memcpy(full_obj+header_len+1,data,len);
 }
 
 // Read an object from the store.
